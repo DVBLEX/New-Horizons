13e4164ecba033a10dca9f8ba85d5a78a9d0c6bd
DVBLEX <ez4rtz322@gmail.com>
2023-12-16 20:58:09 +0200

    New-Horizons with Kotlin and Ktor in Bobocode

---
+++ src/main/kotlin/com/dvblex/Application.kt
@@ -0,0 +1,20 @@
+package com.dvblex
+
+import com.dvblex.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.tomcat.*
+
+fun main() {
+    embeddedServer(Tomcat, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureDatabases()
+    configureSockets()
+    configureRouting()
+}
